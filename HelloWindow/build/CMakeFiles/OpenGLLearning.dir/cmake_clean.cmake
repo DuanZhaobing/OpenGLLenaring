d436c97935bb1907508374ca38eb0f910b3b1ac3
DuanZhaobing <1537976212@163.com>
2024-05-05 19:34:29 +0800

    Hello Window.

---
+++ HelloWindow/build/CMakeFiles/OpenGLLearning.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGLLearning.dir/main.cpp.o"
+  "CMakeFiles/OpenGLLearning.dir/main.cpp.o.d"
+  "OpenGLLearning"
+  "OpenGLLearning.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenGLLearning.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ HelloWindow/cmake-build-debug/CMakeFiles/HelloWindow.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloWindow.dir/main.cpp.o"
+  "CMakeFiles/HelloWindow.dir/main.cpp.o.d"
+  "CMakeFiles/HelloWindow.dir/src/glad.c.o"
+  "CMakeFiles/HelloWindow.dir/src/glad.c.o.d"
+  "HelloWindow"
+  "HelloWindow.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/HelloWindow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
